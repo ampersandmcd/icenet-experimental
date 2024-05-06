c002e6361d3f7d68de3382dd057df21862601943
Andrew McDonald <andrewmcdonald27@gmail.com>
2024-05-06 18:36:08 +0100

    add iterable dataset to load from tfrecords

--- backbone/datasets.py
+++ backbone/datasets.py
@@ -5,72 +5,22 @@ from icenet.data.processors.era5 import IceNetERA5PreProcessor
 from icenet.data.processors.meta import IceNetMetaPreProcessor
 from icenet.data.processors.osi import IceNetOSIPreProcessor
 from icenet.data.loaders import IceNetDataLoaderFactory
+from utils import IterableIceNetDataSetPyTorch
 
 if "datasets.ipynb" in os.listdir():
     os.chdir("../")
 print("Running in {}".format(os.getcwd()))
 
-processing_dates = dict(
-    train=[pd.to_datetime(el) for el in pd.date_range("2020-01-01", "2020-01-01")],
-    val=[pd.to_datetime(el) for el in pd.date_range("2020-01-01", "2020-01-01")],
-    test=[pd.to_datetime(el) for el in pd.date_range("2020-01-01", "2020-01-01")],
-)
-processed_name = "single_day"
-
-pp = IceNetERA5PreProcessor(
-    ["uas", "vas"],
-    ["tas", "zg500", "zg250"],
-    processed_name,
-    processing_dates["train"],
-    processing_dates["val"],
-    processing_dates["test"],
-    linear_trends=tuple(),
-    north=False,
-    south=True
-)
-osi = IceNetOSIPreProcessor(
-    ["siconca"],
-    [],
-    processed_name,
-    processing_dates["train"],
-    processing_dates["val"],
-    processing_dates["test"],
-    linear_trends=tuple(),
-    north=False,
-    south=True
-)
-meta = IceNetMetaPreProcessor(
-    processed_name,
-    north=False,
-    south=True
-)
-pp.init_source_data(
-    lag_days=1,
-)
-pp.process()
-osi.init_source_data(
-    lag_days=1,
-)
-osi.process()
-meta.process()
-
 implementation = "dask"
-loader_config = "loader.single_day.json"
-dataset_name = "single_day"
+dataset_config = "dataset_config.exp23_south.json"
 lag = 1
 
-dl = IceNetDataLoaderFactory().create_data_loader(
-    implementation,
-    loader_config,
-    dataset_name,
-    lag,
-    n_forecast_days=1,
-    north=False,
-    south=True,
-    output_batch_size=1,
-    generate_workers=1
-)
+ds = IterableIceNetDataSetPyTorch(dataset_config, "val", batch_size=4, shuffling=False)
 
-x, y, sw = dl.generate_sample(pd.Timestamp("2020-12-01"))
-diff = np.sum((x[:, :, 2] - y.squeeze())**2)
-print(diff)  # should not be zero
+# x, y, sw = next(iter(ds))
+# print(x)
+
+for i, batch in enumerate(ds):
+    x, y, sw = batch
+    print(x, y, sw)
+    print(i)
--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 import sys
 sys.path.insert(0, '../icenet')  # import local IceNet rather than pip IceNet
 from icenet.data.dataset import IceNetDataSet
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, IterableDataset
 import pandas as pd
 import torch
 from torchmetrics import Metric
+import logging
+from torchdata.datapipes.iter import FileOpener
 
 
 class IceNetDataSetPyTorch(Dataset):
@@ -18,6 +20,7 @@ class IceNetDataSetPyTorch(Dataset):
                                  batch_size=batch_size,
                                  shuffling=shuffling)
         self._dl = self._ds.get_data_loader()
+        self._i = 0
 
         # check mode option
         if mode not in ["train", "val", "test"]:
@@ -25,7 +28,7 @@ class IceNetDataSetPyTorch(Dataset):
         self._mode = mode
 
         self._dates = self._dl._config["sources"]["osisaf"]["dates"][self._mode]
-
+    
     def __len__(self):
         return self._ds._counts[self._mode]
     
@@ -37,6 +40,71 @@ class IceNetDataSetPyTorch(Dataset):
         y = y.squeeze(-1).permute(2, 0, 1)  # collapse repeated dimension and put channel first
         sw = sw.squeeze(-1).permute(2, 0, 1)  # collapse repeated dimension and put channel first
         return x, y, sw
+
+    def get_data_loader(self):
+        return self._ds.get_data_loader()
+    
+    @property
+    def dates(self):
+        return self._dates
+    
+
+class IterableIceNetDataSetPyTorch(IterableDataset):
+    def __init__(self,
+                 configuration_path: str,
+                 mode: str,
+                 batch_size: int = 1,
+                 shuffling: bool = False,
+                ):
+        self._ds = IceNetDataSet(configuration_path=configuration_path,
+                                 batch_size=batch_size,
+                                 shuffling=shuffling)
+        self._dl = self._ds.get_data_loader()
+        self._i = 0
+
+        # check mode option
+        if mode not in ["train", "val", "test"]:
+            raise ValueError("mode must be either 'train', 'val', 'test'")
+        self._mode = mode
+
+        self._dates = self._dl._config["sources"]["osisaf"]["dates"][self._mode]
+
+        # load from tfrecords if processed
+        self._processed = False
+        if (len(self._ds.train_fns) + len(self._ds.val_fns) + len(self._ds.test_fns)) > 0:
+            self._processed = True
+            mode_to_filenames = {"train": self._ds.train_fns, "val": self._ds.val_fns, "test": self._ds.test_fns}
+            filenames = mode_to_filenames[self._mode]
+            datapipe = FileOpener(filenames, mode="b")
+            self._tfrecord_loader_datapipe = iter(datapipe.load_from_tfrecord())
+
+    def __len__(self):
+        return self._ds._counts[self._mode]
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self._processed:
+            for sample in self._tfrecord_loader_datapipe:
+                x, y, sw = sample["x"].numpy(), sample["y"].numpy(), sample["sample_weights"].numpy()
+                x = x.reshape(self._ds.shape + (-1,))
+                y = y.reshape(self._ds.shape + (-1,))
+                sw = sw.reshape(self._ds.shape + (-1,))
+                x, y, sw = torch.from_numpy(x), torch.from_numpy(y), torch.from_numpy(sw)
+                x = x.permute(2, 0, 1)  # put channel to first dimension for pytorch
+                y = y.squeeze(-1).permute(2, 0, 1)  # collapse repeated dimension and put channel first
+                sw = sw.squeeze(-1).permute(2, 0, 1)  # collapse repeated dimension and put channel first
+                return x, y, sw
+        else:
+            x, y, sw = self._dl.generate_sample(date=pd.Timestamp(self._dates[self._i].replace('_', '-')),
+                                        parallel=False)
+            self._i = (self._i + 1) % len(self)
+            x, y, sw = torch.from_numpy(x), torch.from_numpy(y), torch.from_numpy(sw)
+            x = x.permute(2, 0, 1)  # put channel to first dimension for pytorch
+            y = y.squeeze(-1).permute(2, 0, 1)  # collapse repeated dimension and put channel first
+            sw = sw.squeeze(-1).permute(2, 0, 1)  # collapse repeated dimension and put channel first
+            return x, y, sw
 
     def get_data_loader(self):
         return self._ds.get_data_loader()
@@ -72,13 +140,13 @@ class BinaryAccuracy(Metric):
     def update(self, preds: torch.Tensor, target: torch.Tensor, sample_weight: torch.Tensor):
         # preds and target are shape (b, h, w, t)
         # anything greater than 0.15 concentration is ice
-        preds = (preds > 0.15).long()
-        target = (target > 0.15).long()
-        self.correct += torch.sum(preds[:, :, :, self.leadtimes_to_evaluate] == target[:, :, :, self.leadtimes_to_evaluate])
+        bin_preds = torch.where(preds[:, :, :, self.leadtimes_to_evaluate] > 0.15, 1, 0)
+        bin_target = torch.where(target[:, :, :, self.leadtimes_to_evaluate] > 0.15, 1, 0)
+        self.correct += torch.sum(bin_preds == bin_target)
         self.possible += torch.sum(torch.where(sample_weight[:, :, :, self.leadtimes_to_evaluate] > 0, 1, 0))  # binary mask
 
     def compute(self):
-        return self.correct.float() / self.possible
+        return self.correct / self.possible
 
 
 class SIEError(Metric):
@@ -101,17 +169,21 @@ class SIEError(Metric):
         """
         super().__init__()
         self.leadtimes_to_evaluate = leadtimes_to_evaluate
-        self.add_state("pred_sie", default=torch.tensor(0.), dist_reduce_fx="sum")
-        self.add_state("true_sie", default=torch.tensor(0.), dist_reduce_fx="sum")
+        self.add_state("diffs", default=torch.tensor(0.), dist_reduce_fx="sum")
+        self.add_state("forecasts", default=torch.tensor(0.), dist_reduce_fx="sum")
 
     def update(self, preds: torch.Tensor, target: torch.Tensor, sample_weight: torch.Tensor):
         # preds and target are shape (b, h, w, t)
         # anything greater than 0.15 concentration is ice
-        self.pred_sie += torch.sum(torch.where(preds[:, :, :, self.leadtimes_to_evaluate] > 0.15, 1, 0))
-        self.true_sie += torch.sum(torch.where(target[:, :, :, self.leadtimes_to_evaluate] > 0.15, 1, 0))
+        bin_preds = torch.where(preds[:, :, :, self.leadtimes_to_evaluate] > 0.15, 1, 0)
+        bin_target = torch.where(target[:, :, :, self.leadtimes_to_evaluate] > 0.15, 1, 0)
+        # reduce with sign over single field, reduce abs over timesteps and batches
+        self.diffs += torch.sum(torch.abs(torch.sum(bin_preds - bin_target, dim=(1, 2))))
+        b, h, w, t = target.shape
+        self.forecasts += b * t
 
     def compute(self):
-        return (self.pred_sie - self.true_sie) * 25**2 # each pixel is 25x25 km
+        return self.diffs / self.forecasts * 25**2  # each pixel is 25x25 km
     
 
 class SIAError(Metric):
@@ -134,17 +206,21 @@ class SIAError(Metric):
         """
         super().__init__()
         self.leadtimes_to_evaluate = leadtimes_to_evaluate
-        self.add_state("pred_sia", default=torch.tensor(0.), dist_reduce_fx="sum")
-        self.add_state("true_sia", default=torch.tensor(0.), dist_reduce_fx="sum")
+        self.add_state("diffs", default=torch.tensor(0.), dist_reduce_fx="sum")
+        self.add_state("forecasts", default=torch.tensor(0.), dist_reduce_fx="sum")
 
     def update(self, preds: torch.Tensor, target: torch.Tensor, sample_weight: torch.Tensor):
         # preds and target are shape (b, h, w, t)
         # sea ice area is integral of concentration
-        self.pred_sia += torch.sum(preds[:, :, :, self.leadtimes_to_evaluate])
-        self.true_sia += torch.sum(target[:, :, :, self.leadtimes_to_evaluate])
+        preds = preds[:, :, :, self.leadtimes_to_evaluate]
+        target = target[:, :, :, self.leadtimes_to_evaluate]
+        # reduce with sign over single field, reduce abs over timesteps and batches
+        self.diffs += torch.sum(torch.abs(torch.sum(preds - target, dim=(1, 2))))
+        b, h, w, t = target.shape
+        self.forecasts += b * t
 
     def compute(self):
-        return (self.pred_sia - self.true_sia) * 25**2 # each pixel is 25x25 km
+        return self.diffs / self.forecasts * 25**2  # each pixel is 25x25 km
 
 
 class RMSError(Metric):
