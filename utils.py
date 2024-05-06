24e2aca96742e2aae15f3c503dede5f18004cf29
Andrew McDonald <andrewmcdonald27@gmail.com>
2024-05-06 12:56:20 +0100

    proof of concept with large dataset, need to optimise loading

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ from icenet.data.dataset import IceNetDataSet
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 import torch
+from torchmetrics import Metric
+
 
 class IceNetDataSetPyTorch(Dataset):
     def __init__(self,
@@ -42,3 +44,168 @@ class IceNetDataSetPyTorch(Dataset):
     @property
     def dates(self):
         return self._dates
+
+
+class BinaryAccuracy(Metric):
+    """
+    Binary accuracy metric for use at multiple leadtimes.
+    """    
+
+    # Set class properties
+    is_differentiable: bool = False
+    higher_is_better: bool = True
+    full_state_update: bool = True
+
+    def __init__(self, leadtimes_to_evaluate: list):
+        """
+        Construct a binary accuracy metric for use at multiple leadtimes.
+        :param leadtimes_to_evaluate: A list of leadtimes to consider
+            e.g., [0, 1, 2, 3, 4, 5] to consider all six months in accuracy computation or
+            e.g., [0] to only look at the first month's accuracy
+            e.g., [5] to only look at the sixth month's accuracy
+        """
+        super().__init__()
+        self.leadtimes_to_evaluate = leadtimes_to_evaluate
+        self.add_state("correct", default=torch.tensor(0.), dist_reduce_fx="sum")
+        self.add_state("possible", default=torch.tensor(0.), dist_reduce_fx="sum")
+
+    def update(self, preds: torch.Tensor, target: torch.Tensor, sample_weight: torch.Tensor):
+        # preds and target are shape (b, h, w, t)
+        # anything greater than 0.15 concentration is ice
+        preds = (preds > 0.15).long()
+        target = (target > 0.15).long()
+        self.correct += torch.sum(preds[:, :, :, self.leadtimes_to_evaluate] == target[:, :, :, self.leadtimes_to_evaluate])
+        self.possible += torch.sum(torch.where(sample_weight[:, :, :, self.leadtimes_to_evaluate] > 0, 1, 0))  # binary mask
+
+    def compute(self):
+        return self.correct.float() / self.possible
+
+
+class SIEError(Metric):
+    """
+    Sea Ice Extent error metric (in km^2) for use at multiple leadtimes.
+    """ 
+
+    # Set class properties
+    is_differentiable: bool = False
+    higher_is_better: bool = False
+    full_state_update: bool = True
+
+    def __init__(self, leadtimes_to_evaluate: list):
+        """
+        Construct an SIE error metric (in km^2) for use at multiple leadtimes.
+        :param leadtimes_to_evaluate: A list of leadtimes to consider
+            e.g., [0, 1, 2, 3, 4, 5] to consider all six months in computation or
+            e.g., [0] to only look at the first month
+            e.g., [5] to only look at the sixth month
+        """
+        super().__init__()
+        self.leadtimes_to_evaluate = leadtimes_to_evaluate
+        self.add_state("pred_sie", default=torch.tensor(0.), dist_reduce_fx="sum")
+        self.add_state("true_sie", default=torch.tensor(0.), dist_reduce_fx="sum")
+
+    def update(self, preds: torch.Tensor, target: torch.Tensor, sample_weight: torch.Tensor):
+        # preds and target are shape (b, h, w, t)
+        # anything greater than 0.15 concentration is ice
+        self.pred_sie += torch.sum(torch.where(preds[:, :, :, self.leadtimes_to_evaluate] > 0.15, 1, 0))
+        self.true_sie += torch.sum(torch.where(target[:, :, :, self.leadtimes_to_evaluate] > 0.15, 1, 0))
+
+    def compute(self):
+        return (self.pred_sie - self.true_sie) * 25**2 # each pixel is 25x25 km
+    
+
+class SIAError(Metric):
+    """
+    Sea Ice Area error metric (in km^2) for use at multiple leadtimes.
+    """ 
+
+    # Set class properties
+    is_differentiable: bool = False
+    higher_is_better: bool = False
+    full_state_update: bool = True
+
+    def __init__(self, leadtimes_to_evaluate: list):
+        """
+        Construct an SIA error metric (in km^2) for use at multiple leadtimes.
+        :param leadtimes_to_evaluate: A list of leadtimes to consider
+            e.g., [0, 1, 2, 3, 4, 5] to consider all six months in computation or
+            e.g., [0] to only look at the first month
+            e.g., [5] to only look at the sixth month
+        """
+        super().__init__()
+        self.leadtimes_to_evaluate = leadtimes_to_evaluate
+        self.add_state("pred_sia", default=torch.tensor(0.), dist_reduce_fx="sum")
+        self.add_state("true_sia", default=torch.tensor(0.), dist_reduce_fx="sum")
+
+    def update(self, preds: torch.Tensor, target: torch.Tensor, sample_weight: torch.Tensor):
+        # preds and target are shape (b, h, w, t)
+        # sea ice area is integral of concentration
+        self.pred_sia += torch.sum(preds[:, :, :, self.leadtimes_to_evaluate])
+        self.true_sia += torch.sum(target[:, :, :, self.leadtimes_to_evaluate])
+
+    def compute(self):
+        return (self.pred_sia - self.true_sia) * 25**2 # each pixel is 25x25 km
+
+
+class RMSError(Metric):
+    """
+    RMSE for use at multiple leadtimes.
+    """
+
+    # Set class properties
+    is_differentiable: bool = True
+    higher_is_better: bool = False
+    full_state_update: bool = True
+
+    def __init__(self, leadtimes_to_evaluate: list):
+        """
+        Construct an RMSE error metric (in km^2) for use at multiple leadtimes.
+        :param leadtimes_to_evaluate: A list of leadtimes to consider
+            e.g., [0, 1, 2, 3, 4, 5] to consider all six months in computation or
+            e.g., [0] to only look at the first month
+            e.g., [5] to only look at the sixth month
+        """
+        super().__init__()
+        self.leadtimes_to_evaluate = leadtimes_to_evaluate
+        self.add_state("diffs", default=torch.tensor(0.), dist_reduce_fx="sum")
+        self.add_state("pixels", default=torch.tensor(0.), dist_reduce_fx="sum")
+
+    def update(self, preds: torch.Tensor, target: torch.Tensor, sample_weight: torch.Tensor):
+        # preds and target are shape (b, h, w, t)
+        self.diffs += torch.sum((preds[:, :, :, self.leadtimes_to_evaluate] - target[:, :, :, self.leadtimes_to_evaluate])**2)
+        self.pixels += torch.sum(torch.where(sample_weight[:, :, :, self.leadtimes_to_evaluate] > 0, 1, 0))
+
+    def compute(self):
+        return torch.sqrt(self.diffs / self.pixels)
+
+
+class MAError(Metric):
+    """
+    MAE for use at multiple leadtimes.
+    """
+
+    # Set class properties
+    is_differentiable: bool = True
+    higher_is_better: bool = False
+    full_state_update: bool = True
+
+    def __init__(self, leadtimes_to_evaluate: list):
+        """
+        Construct an MAE error metric (in km^2) for use at multiple leadtimes.
+        :param leadtimes_to_evaluate: A list of leadtimes to consider
+            e.g., [0, 1, 2, 3, 4, 5] to consider all six months in computation or
+            e.g., [0] to only look at the first month
+            e.g., [5] to only look at the sixth month
+        """
+        super().__init__()
+        self.leadtimes_to_evaluate = leadtimes_to_evaluate
+        self.add_state("diffs", default=torch.tensor(0.), dist_reduce_fx="sum")
+        self.add_state("pixels", default=torch.tensor(0.), dist_reduce_fx="sum")
+
+    def update(self, preds: torch.Tensor, target: torch.Tensor, sample_weight: torch.Tensor):
+        # preds and target are shape (b, h, w, t)
+        self.diffs += torch.sum(torch.abs(preds[:, :, :, self.leadtimes_to_evaluate] - target[:, :, :, self.leadtimes_to_evaluate]))
+        self.pixels += torch.sum(torch.where(sample_weight[:, :, :, self.leadtimes_to_evaluate] > 0, 1, 0))
+
+    def compute(self):
+        return self.diffs / self.pixels
