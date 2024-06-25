80757c5ee74df9adc1a47437ec9c048d03ed0142
Andrew McDonald <andrewmcdonald27@gmail.com>
2024-06-25 14:40:05 +0100

    add single year single lag single lead dataset

--- backbone-v1/datasets.py
+++ backbone-v1/datasets.py
@@ -1,12 +1,8 @@
 import os
 import pandas as pd
 import numpy as np
-from icenet.data.processors.era5 import IceNetERA5PreProcessor
-from icenet.data.processors.meta import IceNetMetaPreProcessor
-from icenet.data.processors.osi import IceNetOSIPreProcessor
-from icenet.data.loaders import IceNetDataLoaderFactory
-from utils import IterableIceNetDataSetPyTorch
 import torch
+from utils import IterableIceNetDataSetPyTorch
 
 if "datasets.ipynb" in os.listdir():
     os.chdir("../")
@@ -17,11 +13,9 @@ dataset_config = "dataset_config.exp23_south.json"
 lag = 1
 
 ds = IterableIceNetDataSetPyTorch(dataset_config, "test", batch_size=4, shuffling=False)
-# dl = torch.utils.data.DataLoader(ds, batch_size=4, shuffle=False)
-# x, y, sw = next(iter(ds))
-# print(x)
+dl = torch.utils.data.DataLoader(ds, batch_size=4, shuffle=False)
 
-for i, batch in enumerate(ds):
+for i, batch in enumerate(dl):
     x, y, sw = batch
     print(x, y, sw)
     print(i)
